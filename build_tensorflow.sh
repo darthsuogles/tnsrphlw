e0304b09ca15fe14eb43fdd082f93b76a4feacc8
Philip Yang <darthsuogle@gmail.com>
2018-01-01 22:10:59 -0800

    new build method

--- build_tensorflow.sh
+++ build_tensorflow.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 __py_ver=${1:-3}
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
-NUKE_EXISTING=yes
-BRANCH=r1.1
+NUKE_EXISTING=no
+BRANCH=master
 
-set -ex
+set -euxo pipefail
 
 # Python settings
-export PY_VER=${__py_ver}
+export PY_VER="${__py_ver}"
 export PYTHON_PKG_PREFIX="$(python${PY_VER}-config --prefix)"
 export PYTHON_BIN_PATH="$(which python${PY_VER})"
-export PYTHON="$(which python${PY_VER})"
-export PIP="$(which pip${PY_VER})"
+export PYTHON="${PYTHON_BIN_PATH}"
+export PIP="${PYTHON} -m pip"
 
-if [ "darwin" == "${OS}" ]; then
+if [[ "darwin" == "${OS}" ]]; then
     brew upgrade bazel swig || brew install bazel swig
 else
-    module load linuxbrew bazel python
+    module load linuxbrew bazel python || echo "environment modules failed to load"
     # if [[ "2" == "${PY_VER}" ]]; then
     #     module load python
     # else
@@ -29,10 +29,11 @@ else
                            uniq)
 fi
 
-"${PIP}" install -U six numpy wheel
+${PIP} install -U six numpy wheel
 git submodule update --init --recursive --remote
 (cd dev; echo "Update google/tensorflow repo"
- git reset --hard "origin/${BRANCH}"
+ # Ignore reset for the time being
+ #git reset --hard "origin/${BRANCH}"
  
  export USE_DEFAULT_PYTHON_LIB_PATH=1
  export TF_NEED_GCP=0
@@ -40,27 +41,28 @@ git submodule update --init --recursive --remote
  export TF_NEED_CUDA=0
  export TF_NEED_OPENCL=0
  export TF_NEED_JEMALLOC=1
+ export TF_NEED_S3=1
+ export TF_NEED_GDR=0
  export TF_ENABLE_XLA=1
- export TF_NEED_MKL=0
+ export TF_NEED_MKL=1
+ export TF_NEED_MPI=0
  export TF_NEED_VERBS=0
- export PYTHON_BIN_PATH="$(which python3)"
+ export TF_NEED_OPENCL_SYCL=0
+ export TF_SET_ANDROID_WORKSPACE=0
+ 
  export CC_OPT_FLAGS="-march=native"
  ./configure
 
  # Installing as a python package
- [ "yes" == "${NUKE_EXISTING}" ] && bazel clean --expunge
- bazel build -c opt --copt=-march=native //tensorflow/tools/pip_package:build_pip_package
+ [[ "yes" == "${NUKE_EXISTING}" ]] && bazel clean --expunge
+ bazel build \
+       --config=mkl \
+       --copt=-march=native \
+       //tensorflow/tools/pip_package:build_pip_package
  
  tf_pkg_dir="/tmp/tensorflow_pkg"
  rm -fr "${tf_pkg_dir}" && mkdir -p "${tf_pkg_dir}"
  ./bazel-bin/tensorflow/tools/pip_package/build_pip_package "${tf_pkg_dir}"
- "${PIP}" uninstall -y tensorflow
- find "${tf_pkg_dir}/" -name 'tensorflow-*.whl' -exec "${PIP}" install {} \;
-
- # # Building locally for development
- # rm -fr _python_build && mkdir _python_build
- # cd _python_build
- # ln -s ../bazel-bin/tensorflow/tools/pip_package/build_pip_package.runfiles/* .
- # ln -s ../tensorflow/tools/pip_package/* .
- # "${PYTHON}" setup.py develop
+ ${PIP} uninstall -y tensorflow
+ find "${tf_pkg_dir}/" -name 'tensorflow-*.whl' -exec ${PIP} install -U {} \;
 )
